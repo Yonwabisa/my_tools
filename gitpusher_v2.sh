49e827a20f6318311103eac437422c6cc6b059ca
Yonwabisa <yonwabisamphunga.yz@gail.com>
2020-03-13 11:15:47 +0200

    added color and SKIP option, to avoid overwriting HEAD contents

---
+++ gitpusher_v2.sh
@@ -0,0 +1,51 @@
+DULL='\033[0m'
+GREEN='\033[5;32m'
+CYAN='\033[5;36m'
+
+func_commit()
+{
+echo "commit: "
+read mymessage
+git commit -m "$mymessage"
+}
+
+func_add()
+{
+echo "\a\nYou can type \"${GREEN}SKIP${DULL}\" to move on to the commit stage"
+echo "Or you can type \"${CYAN}EXIT${DULL}\" to exit the script\n"
+echo git add: 
+read myfile
+if [ $myfile = 'SKIP' ]
+then
+	func_commit
+else
+	git add $myfile
+	func_commit
+fi
+}
+
+clear
+
+ls -a -l
+
+git status
+
+func_add
+
+clear
+git status
+
+echo "push? [y/n]"
+
+read myconfirmation
+
+if [ $myconfirmation = 'y' ] || [ $myconfirmation = 'Y' ] || [ $myconfirmation = 'yes' ] || [ $myconfirmation = 'Yes' ]
+then
+		echo "Attempting push\n"
+		git push origin master
+		git log
+elif [ $myconfirmation = 'n' ] || [ $myconfirmation = 'N' ] || [ $myconfirmation = 'no' ] || [ $myconfirmation = 'No' ]
+then
+		echo "not pushed\n"
+		git reset $myfile
+fi
