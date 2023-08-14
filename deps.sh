d526848ffc977b7bb5084f5cbcb1be8848d43def
yonwabisa <yonwabisamphunga.yz@gmail.com>
2023-08-14 17:13:43 +0200

    added more homebrew and a status line

--- deps.sh
+++ deps.sh
@@ -4,30 +4,7 @@ sudo dnf makecache
 # Shells and stuff
 
 sudo dnf install zsh dash -y
-ln -sfT dash /usr/bin/sh
 
-# kitty terminal
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-# Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in your PATH)
-sudo mkdir -p ~/.local/bin
-ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
-# Place the kitty.desktop file somewhere it can be found by the OS
-cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
-# Update the path to the kitty icon in the kitty.desktop file
-sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty.desktop
-
-#==========================================================================================================================================================
-# More Fedora repositories
-
-#rpm fusion
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-
-#albert
-sudo rpm --import "https://build.opensuse.org/projects/home:manuelschneid3r/public_key"
-sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:manuelschneid3r/Fedora_34/home:manuelschneid3r.repo
-
- 
 # #==========================================================================================================================================================
 # Audio and Video stuff
  
@@ -78,41 +55,33 @@ sudo dnf install -y ImageMagick
 cp ./.p10k.zsh $HOME
 cp ./.zshrc $HOME
 
-# #==========================================================================================================================================================
-# Editors and other extras.. and stuff
-
-yes | sudo dnf copr enable agriffis/neovim-nightly
-sudo dnf install -y neovim python3-neovim
-
-# installing npm
-
-sudo dnf install -y  npm
-
-# make cache folder (if missing) and take ownership
-sudo mkdir -p /usr/local/n
-sudo chown -R $(whoami) /usr/local/n
-# make sure the required folders exist (safe to execute even if they already exist)
-sudo mkdir -p /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
-# take ownership of Node.js install destination folders
-sudo chown -R $(whoami) /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
-
-npm install -g n
-npm install -g typescript
-#npm install -g @angular/cli
-
-# Neovim vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 mkdir ~/.ssh
 pushd $HOME/.ssh
 yes | ssh-keygen -t rsa -b 8192 -f git -N ''
 popd
 
-sudo dnf install -y snapd
-sudo ln -s /var/lib/snapd/snap /snap
-
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+git clone https://github.com/nwg-piotr/autotiling.git
+chmod +x ./autotiling/main.py
+mv ./autotiling/main.py ~/.bin/autotiling
+
+pip install --user bumblebee-status
+sudo dnf install -y dunst gnome-system-monitor python-pulsectl xset
+
+
+# Get Homebrew
+
+git clone https://github.com/Homebrew/install.git
+/bin/bash -c ./install/install.sh
+(echo; echo eval export HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew";
+export HOMEBREW_CELLAR="/home/linuxbrew/.linuxbrew/Cellar";
+export HOMEBREW_REPOSITORY="/home/linuxbrew/.linuxbrew/Homebrew";
+export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin${PATH+:$PATH}";
+export MANPATH="/home/linuxbrew/.linuxbrew/share/man${MANPATH:$MANPATH};";
+export INFOPATH="/home/linuxbrew/.linuxbrew/share/infor:${INFOPATH:-}";) >> /home/$USER/.zprofile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# Getting nice terminal theme-ey stuff from brew
+brew install lsd
